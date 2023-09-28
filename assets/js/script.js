fd93a9e0ac16f256cb1df1e5e9a5f050c236ffe3
nkartikaxioned <kartikn@axioned.com>
2023-09-28 18:36:04 +0530

    blurry loading js feature add

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,31 @@
 
 */
 
+const background = document.querySelector('.blurry_loading'),
+  incrementText = document.querySelector('.incremental_text');
+
+window.addEventListener('load', () => {
+  let count = 0,
+    maxcount = 100;
+  let textInterval = setInterval(() => {
+    count++;
+    incrementText.innerHTML = `${count}%`;
+    if (count === maxcount) {
+      clearInterval(textInterval);
+    }
+    incrementText.style.opacity = scale(count, 0, 100, 1, 0)
+    background.style.filter = `blur(${scale(count, 0, 100, 10, 0)}px)`
+  }, 65)
+
+  const scale = (num, minval, maxvalue, valueoutmin, valueoutmax) => {
+    return ((num - minval) * (valueoutmax - valueoutmin)) / (maxvalue - minval) + valueoutmin
+  }
+})
 
 
-
-
-
+// const scale = (num, in_min, in_max, out_min, out_max) => {
+//   return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+// }
 
 
 
